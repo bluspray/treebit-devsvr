39d45bebea3502d37f8308e058bec828896e8cf1
bluspray <yellowspray@gmail.com>
2025-12-02 10:30:46 +0900

    Fix collector import by making backend a package

--- backend/app/main.py
+++ backend/app/main.py
@@ -9,7 +9,7 @@ from fastapi.middleware.cors import CORSMiddleware
 from fastapi.responses import RedirectResponse
 from fastapi.staticfiles import StaticFiles
 from pydantic import BaseModel, Field
-from . import collector
+from .. import collector
 
 
 class ServerInput(BaseModel):
